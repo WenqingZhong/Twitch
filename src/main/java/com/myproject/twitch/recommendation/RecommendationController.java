2698c2c7b7c5c12d4f9c66389f27be4a3ac8d057
WenqingZhong <zhongwenqing99@gmail.com>
2023-06-15 18:44:27 -0400

    recommendation implementation

---
+++ src/main/java/com/myproject/twitch/recommendation/RecommendationController.java
@@ -0,0 +1,33 @@
+package com.myproject.twitch.recommendation;
+
+import com.myproject.twitch.db.entity.UserEntity;
+import com.myproject.twitch.model.TypeGroupedItemList;
+import com.myproject.twitch.user.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationController {
+
+
+    private final RecommendationService recommendationService;
+    private final UserService userService;
+
+
+    public RecommendationController(RecommendationService recommendationService, UserService userService) {
+        this.recommendationService = recommendationService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/recommendation")
+    public TypeGroupedItemList getRecommendation(@AuthenticationPrincipal User user) {
+        UserEntity userEntity = null;
+        if (user != null) {
+            userEntity = userService.findByUsername(user.getUsername());
+        }
+        return recommendationService.recommendItems(userEntity);
+    }
+}
+
